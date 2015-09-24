b66b116b30a33a013adb811991149b861ee86cd6
pathmakumara <mgcp.rs@gmail.com>
2015-09-24 23:49:22 +0900

    well done

--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,6 @@ module Etsydemo
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-    config.i18n.available_locales = %i(ja en zh)
-    config.i18n.enforce_available_locales = true
     config.i18n.default_locale = :ja
   end
 end
